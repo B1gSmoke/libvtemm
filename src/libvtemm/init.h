8f49660734d7cd1daa48166e88866cba217a4d67
krnowak <krnowak@localhost>
2009-03-17 21:57:23 +0000

    New import.
    svn path=/trunk/; revision=2

---
+++ src/libvtemm/init.h
@@ -0,0 +1,38 @@
+/* init.h
+ *
+ * Copyright (C) 2008 libvtemm Development Team
+ *
+ * This file is part of libvtemm.
+ *
+ * libvtemm is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libvtemm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with libvtemm.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _LIBVTEMM_INIT_H_
+#define _LIBVTEMM_INIT_H_
+
+#include <libvtemmconfig.h>
+
+namespace Gnome
+{
+
+namespace Vte
+{
+
+void init();
+
+} //namespace Vte
+
+} //namespace Gnome
+
+#endif // _LIBVTEMM_INIT_H_
